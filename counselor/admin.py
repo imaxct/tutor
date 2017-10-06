216a145d9cc7ac5651595c643d373bb5a8d7179a
imaxct <imaxct@foxmail.com>
2017-10-06 22:30:58 +0800

    添加实体, admin管理中注册Question.

--- counselor/admin.py
+++ counselor/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+
+class OptionInline(admin.StackedInline):
+    model = Option
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': 'question_text'}),
+        (u'发布日期', {'fields': 'pub_date'})
+    ]
+    inlines = [OptionInline]
+
+
+admin.site.register(Question, QuestionAdmin)
--- counselor/models.py
+++ counselor/models.py
@@ -8,6 +8,8 @@ class User(models.Model):
     """
     用户
     """
+    username = models.CharField(max_length=20)
+    # TODO add fields
     pass
 
 
@@ -15,18 +17,38 @@ class Record(models.Model):
     """
     记录
     """
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='record user')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='record question')
+    option = models.ForeignKey(Option, on_delete=models.CASCADE, verbose_name='record option')
+
+    class Meta:
+        indexes = [
+            models.Index(fields=[])
+        ]
+
+    def __str__(self):
+        return '%s %s %s' % (self.user.username, self.question.question_text, self.option.option_num)
 
 
-class Problem(models.Model):
+class Question(models.Model):
     """
     问题
     """
-    pass
+    pub_date = models.DateField
+    question_text = models.TextField
+
+    def __str__(self):
+        return self.question_text
 
 
 class Option(models.Model):
     """
     选项
     """
-    pass
+    score = models.FloatField
+    option_text = models.CharField(max_length=200)
+    option_num = models.CharField(max_length=1)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='question id')
+
+    def __str__(self):
+        return self.option_text
